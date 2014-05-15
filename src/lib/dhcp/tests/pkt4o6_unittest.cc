2de725af92165f05910ab634c748ad6d8e4fc2b0
wyqbupt <wyqcsnet@yeah.net>
2014-05-15 10:43:37 +0800

    [4o6]create CreatePkt4o6_case1() function

--- src/lib/dhcp/tests/pkt4o6_unittest.cc
+++ src/lib/dhcp/tests/pkt4o6_unittest.cc
@@ -55,20 +55,23 @@ public:
     }
 };
 
-//test Pkt4o6 class constructor
-TEST_F(Pkt4o6Test, constructor) {
+const int dataLength = 250;
 
-
+Pkt4o6Ptr CreatePkt4o6_case1(){
     // test data array
-    uint8_t testData[250];
-    for (int i = 0; i < 250; i++) {
+    uint8_t testData[dataLength];
+    for (int i = 0; i < dataLength; i++) {
         testData[i] = i;
     }
-    //Case 1:test Pkt4o6::Pkt4o6(const Pkt6Ptr& pkt6)
 	//construct a Pkt4o6
-	Pkt4Ptr pktv4(new Pkt4(testData,250));
-    Pkt6Ptr pktv6(new Pkt6(testData,240));
-	pktv6->setIndex(1);
+	Pkt4Ptr pktv4(new Pkt4(testData,dataLength));
+    Pkt6Ptr pktv6(new Pkt6(testData,dataLength));
+	pktv6->setRemotePort(546);
+    pktv6->setRemoteAddr(IOAddress("fe80::21e:8cff:fe9b:7349"));
+    pktv6->setLocalPort(0);
+    pktv6->setLocalAddr(IOAddress("ff02::1:2"));
+    pktv6->setIndex(2);
+    pktv6->setIface("eth0");
 	pktv4->repack();
 	isc::util::OutputBuffer tmp = pktv4->getBuffer();
     OptionBuffer p((uint8_t*)tmp.getData(),
@@ -76,7 +79,25 @@ TEST_F(Pkt4o6Test, constructor) {
     OptionPtr opt = OptionPtr(new Option(Option::V6, OPTION_DHCPV4_MSG, p));
     pktv6->addOption(opt);
 	Pkt4o6Ptr pkt4o6(new Pkt4o6(pktv6));
-	//end of construct a Pkt4o6
+	return pkt4o6;
+}
+
+Pkt4o6Ptr CreatePkt4o6_case2(){
+    // test data array
+    uint8_t testData[dataLength];
+    for (int i = 0; i < dataLength; i++) {
+        testData[i] = i;
+    }
+    Pkt4o6Ptr pkt4o6(new Pkt4o6(testData,dataLength,testData,dataLength));
+	return pkt4o6;
+}
+
+//test Pkt4o6 class constructor
+TEST_F(Pkt4o6Test, constructor) {
+
+
+    //Case 1:test Pkt4o6::Pkt4o6(const Pkt6Ptr& pkt6)
+    Pkt4o6Ptr pkt4o6 = CreatePkt4o6_case1();
 
 	//now we test original data,create Pkt4 and Pkt6
  	const isc::util::OutputBuffer pkt4_buf = pkt4o6->getPkt4()->getBuffer();
@@ -87,24 +108,24 @@ TEST_F(Pkt4o6Test, constructor) {
 	v6->repack();
     const isc::util::OutputBuffer &buf4(v4->getBuffer());
     const isc::util::OutputBuffer &buf6(v6->getBuffer());
-	ASSERT_EQ(250, buf4.getLength());
-	ASSERT_EQ(240, buf6.getLength());//test current length
+	ASSERT_EQ(dataLength, buf4.getLength());
+	ASSERT_EQ(dataLength, buf6.getLength());//test current length
 	uint8_t* data = (uint8_t*)buf4.getData();
-	for(int i = 0;i < 10;i++){
+	for(int i = 0;i < 100;i++){
 		EXPECT_EQ(i,data[i]);
 	}
 
 	//Case 2: test Pkt4o6::Pkt4o6(const uint8_t* data4, size_t len4, 
 	//                            const uint8_t* data6, size_t len6)
-	Pkt4o6Ptr pkt4o6_(new Pkt4o6(testData,240,testData,250));
-	ASSERT_EQ(240, pkt4o6_->getPkt4()->getBuffer().getLength());
-	ASSERT_EQ(250, pkt4o6_->getPkt6()->getBuffer().getLength());
+	Pkt4o6Ptr pkt4o6_ = CreatePkt4o6_case2();
+	ASSERT_EQ(dataLength, pkt4o6_->getPkt4()->getBuffer().getLength());
+	ASSERT_EQ(dataLength, pkt4o6_->getPkt6()->getBuffer().getLength());
 	data = (uint8_t*)pkt4o6_->getPkt4()->getBuffer().getData();
-	for(int i = 0;i < 10;i++){
+	for(int i = 0;i < 100;i++){
 	    EXPECT_EQ(i,data[i]);
 	}
 	data = (uint8_t*)pkt4o6_->getPkt6()->getBuffer().getData();
-	for(int i = 0;i < 10;i++){
+	for(int i = 0;i < 100;i++){
 	    EXPECT_EQ(i,data[i]);
 	}
 	
@@ -123,45 +144,21 @@ TEST_F(Pkt4o6Test, constructor) {
 	pkt4_third_->repack();
 	pkt6_third_->repack();
 	data = (uint8_t*)pkt4_third_->getBuffer().getData();
-	for(int i = 0;i < 10;i++)
+	for(int i = 0;i < 100;i++)
 	    EXPECT_EQ(i+2,data[i]);
 	data = (uint8_t*)pkt6_third_->getBuffer().getData();
-	for(int i = 0;i < 10;i++)
+	for(int i = 0;i < 100;i++)
 	    EXPECT_EQ(i+2,data[i]);
 }
 
 //test setJson and getJson 
 TEST_F(Pkt4o6Test, jsontest) {
 
-    //create Pkt4o6
-    uint8_t data[] = { 0, 1, 2, 3, 4, 5 };
-    Pkt6Ptr pktv6(new Pkt6(data, sizeof(data)));
-    pktv6->setRemotePort(546);
-    pktv6->setRemoteAddr(IOAddress("fe80::21e:8cff:fe9b:7349"));
-    pktv6->setLocalPort(0);
-    pktv6->setLocalAddr(IOAddress("ff02::1:2"));
-    pktv6->setIndex(2);
-    pktv6->setIface("eth0");
-    uint8_t testData[250];
-    for (int i = 0; i < 250; i++) {
-        testData[i] = i;
-    }
-    
-    Pkt4Ptr pktv4(new Pkt4(testData,250));
-    pktv4->repack();
-    isc::util::OutputBuffer tmp = pktv4->getBuffer();
-    OptionBuffer opt_buf((uint8_t*)tmp.getData(),
-	                     (uint8_t*)tmp.getData()+tmp.getLength());
-    OptionPtr opt = OptionPtr(new Option(Option::V6, 
-                                        OPTION_DHCPV4_MSG, opt_buf));
-    pktv6->addOption(opt);
-	Pkt4o6Ptr pkt4o6(new Pkt4o6(pktv6));
-	//end of create Pkt4o6
+	Pkt4o6Ptr pkt4o6 = CreatePkt4o6_case1();
 	
 	//store pkt4o6 josn info.
 	std::string json = pkt4o6->getJson();
-	std::cout<<json<<std::endl;
-	Pkt4o6Ptr pkt4o6_(new Pkt4o6(data,250,testData,250));
+	Pkt4o6Ptr pkt4o6_ = CreatePkt4o6_case2();
 	pkt4o6_->setJson(json);
 	int RemotePort = 546; 
 	std::string RemoteAddr("fe80::21e:8cff:fe9b:7349");
@@ -182,29 +179,13 @@ TEST_F(Pkt4o6Test, jsontest) {
 
 //test DHCPv4MsgOption
 TEST_F(Pkt4o6Test, DHCPv4MsgOption) {
-    uint8_t data[] = { 0, 1, 2, 3, 4, 5 };
-    Pkt6Ptr pktv6(new Pkt6(data, sizeof(data)));
-    uint8_t testData[250];
-    for (int i = 0; i < 250; i++) {
-      		testData[i] = i;
-    }
-	Pkt4Ptr pktv4(new Pkt4(testData,250));
-    pktv4->repack();
-	isc::util::OutputBuffer tmp = pktv4->getBuffer();
-	OptionBuffer opt_buf((uint8_t*)tmp.getData(),
-	                     (uint8_t*)tmp.getData()+tmp.getLength());
-    OptionPtr opt = OptionPtr(new Option(Option::V6, 
-                              OPTION_DHCPV4_MSG, opt_buf));
-    pktv6->addOption(opt);
-	Pkt4o6Ptr pkt4o6(new Pkt4o6(pktv6));
+	Pkt4o6Ptr pkt4o6 = CreatePkt4o6_case1();
 	
 	//get DHCPv4MsgOption from Pkt4o6
 	OptionBuffer buf(pkt4o6->getDHCPv4MsgOption());
-	EXPECT_EQ(0,buf[0]);
-	EXPECT_EQ(1,buf[1]);
-	EXPECT_EQ(2,buf[2]);
-	EXPECT_EQ(3,buf[3]);
-	EXPECT_EQ(4,buf[4]);
+	for(int i = 0;i < dataLength;i++)
+	    EXPECT_EQ(i,buf[i]);
+	
 
 }
 
