468d8641bbbd9cdc0df06cea68e3ed1a3b2f488e
gnocuil <gnocuil@gmail.com>
2014-05-18 23:53:35 +0800

    add comments in DHCP4o6IPC

--- src/lib/dhcp/dhcp4o6_ipc.h
+++ src/lib/dhcp/dhcp4o6_ipc.h
@@ -30,46 +30,120 @@ public:
         isc::Exception(file, line, what) { };
 };
 
+/// @brief IPC class to pass Pkt4o6 between DHCPv4 and DHCPv6 servers
 class DHCP4o6IPC : public isc::util::BaseIPC {
 public:
+    /// @brief Default constructor.
+    ///
+    /// DHCP4o6IPC is expected to create only one instance in a process,
+    /// and will set itself to intances_ in constructor
     DHCP4o6IPC() { instance_ = this; }
+    
+    /// @brief Destructor.
     virtual ~DHCP4o6IPC() { instance_ = NULL; }
+    
+    /// @brief create and initialize sockets to corresponding addresses
+    ///
+    /// this function calls methods in BaseIPC for socket processing
+    /// Method will throw if BaseIPC methods failed
     void open();
+    
+    /// @brief send a DHCPv4 ove DHCPv6 packet
+    ///
+    /// this function converts Pkt4o6 into binary data and sends it
+    /// through BaseIPC send() 
+    /// Method will throw if BaseIPC send() failed
+    ///
+    /// @param pkt4o6 Pointer of the packet to be sent
     void sendPkt4o6(const Pkt4o6Ptr& pkt4o6);
+    
+    /// @brief receive a DHCPv4 ove DHCPv6 packet
+    ///
+    /// this function calls BaseIPC recv() to receive binary data
+    /// and converts it into Pkt4o6
+    /// Received Pkt4o6 will be push into a queue and not returned
+    /// Method will throw if BaseIPC recv() failed or Pkt4o6
+    /// construction failed
     void recvPkt4o6();
+    
+    /// @brief callback function for IfaceMgr
+    ///
+    /// This function is called when IfaceMgr select() has data from
+    /// DHCP4o6IPC socket. It calls recvPkt4o6() to receive data.
     static void callback();
+    
+    /// @brief test if receive queue is empty
+    /// 
+    /// @return true if queue is empty
     bool empty() const { return queue_.empty(); }
+    
+    /// @brief retrive and remove a Pkt4o6 in the receive queue
+    ///
+    /// @return if not empty, Pkt4o6 in the head of the queue; otherwise
+    /// return a null pointer
     Pkt4o6Ptr pop();
+    
+    /// @brief check if a given pkt4 is the current processing pkt4o6
     bool isCurrent(Pkt4Ptr pkt4) {
         return current_ && pkt4 == current_->getPkt4();
     }
+    
+    /// @brief check if a given pkt6 is the current processing pkt4o6
     bool isCurrent(Pkt6Ptr pkt6) {
         return current_ && pkt6 == current_->getPkt6();
     }
+    
+    /// @brief get current DHCP4o6IPC instance
     Pkt4o6Ptr current() { return current_; }
+    
+    /// @brief enable DHCP4o6 function
+    ///
+    /// will add external socket into IfaceMgr
     static void enable();
+    
+    /// @brief enable DHCP4o6 function
+    ///
+    /// will remove external socket from IfaceMgr
     static void disable();
 protected:
+
+    /// @brief a (local) filename to listen to
+    ///
+    /// @return a string of filename
     virtual std::string getLocalFilename() const = 0;
+    
+    /// @brief a (remote) filename to send packets to
+    ///
+    /// @return a string of filename
     virtual std::string getRemoteFilename() const = 0;
+    
+    /// @brief a queue of received DHCPv4 over DHCPv6 packets that has
+    /// not been processed
     std::queue<Pkt4o6Ptr> queue_;
     
     /// @brief Static pointer to the sole instance of the DHCP4o6 IPC.
     ///
-    /// This is required for receive callback in iface_mgr
+    /// This is required by callback function of iface_mgr
     static DHCP4o6IPC* instance_;
+    
+    /// @brief the current processing DHCPv4 over DHCPv6 packet
     Pkt4o6Ptr current_;
 };//DHCP4o6IPC class
 
+// A filename used for DHCPv4 server --> DHCPv6 server
 #define FILENAME_4TO6 "DHCPv4_over_DHCPv6_v4tov6";
+
+// A filename used for DHCPv4 server <-- DHCPv6 server
 #define FILENAME_6TO4 "DHCPv4_over_DHCPv6_v6tov4";
 
+/// @brief IPC class actually used in DHCPv4 server
 class DHCP4IPC : public DHCP4o6IPC {
 protected:
     virtual std::string getLocalFilename() const { return FILENAME_6TO4; }
     virtual std::string getRemoteFilename() const { return FILENAME_4TO6; }
 };
 
+/// @brief IPC class actually used in DHCPv6 server
 class DHCP6IPC : public DHCP4o6IPC {
 protected:
     virtual std::string getLocalFilename() const { return FILENAME_4TO6; }
